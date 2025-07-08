790fa445d9d3fdcbcfbc26cad267b38062a475a8
Samueelljr <samueldejesus705@gmail.com>
2025-07-07 22:41:31 -0300

    logica para mudança de componentes

--- logica.js
+++ logica.js
@@ -1,34 +1,25 @@
-// const redirectCalculationHouseofWork = () => {
-//     window.location.replace(Calculo.html)
-// }
+let passoAtual = 1;
+const totalPassos = 3;
 
-// function enviar () {
-//     document.getElementById('meu-formulario').addEventListener('submit', function(e) {
-//         e.preventDefault();
-//         window.location.href = 'calculo.html'
-//     })
-// }
-
-function nextComponent() {
-  document.getElementById("form").addEventListener("click", function (event) {
-    event.preventDefault();
-    const comp1 = document.getElementById("component1");
-    const comp2 = document.getElementById("component2");
-    const comp3 = document.getElementById("component3");
-
-    if (comp1.classList.contains("active")) {
-      comp1.classList.remove("active");
-      comp1.classList.add("disable");
-      comp2.classList.add("active");
+function nextComponent(passo) {
+  for (let i = 1; i <= totalPassos; i++) {
+    const componente = document.getElementById(`component${i}`);
+    if (componente) {
+      componente.classList.remove('active');
+      componente.classList.add('component');
     }
-  });
+  }
+
+  const componenteAtivo = document.getElementById(`component${passo}`);
+  if (componenteAtivo) {
+    componenteAtivo.classList.remove('component');
+    componenteAtivo.classList.add('active');
+  }
 }
 
-function cal() {
-  const inputValue1 = document.getElementById("distanciaKm");
-  const value1 = inputValue1.value;
-  const inputValue2 = document.getElementById("consumoMedio");
-  const value2 = inputValue2.value;
-  const resultado = value1 + value2;
-  return resultado;
+function next() {
+  if (passoAtual < totalPassos) {
+    passoAtual++;
+    nextComponent(passoAtual)
+  } return
 }
